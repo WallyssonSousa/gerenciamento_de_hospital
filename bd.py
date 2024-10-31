f1b2e4bd1b7343f215fe8879fe9e45484a3a5aec
NicolasDiasReis <nico.reisdias@example.com>
2024-10-31 09:26:33 -0300

    Colunas

--- bd.py
+++ bd.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey, DateTime
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 db = create_engine("sqlite:///bancoteste.db")
 Session = sessionmaker(bind=db)
@@ -7,84 +7,85 @@ session = Session()
 
 Base = declarative_base()
 
-#Tabelas
+# Tabelas
+#===================================================================================================
 
-#============================PROONTUARIO==============================================
 class Prontuario(Base):
     __tablename__ = "prontuario"
-
     id = Column("id", Integer, primary_key=True, autoincrement=True)
-#======================================================================================
+#=================================================================================================
 
-
-#==============================PACIENTES===============================================
 class Paciente(Base):
     __tablename__ = "paciente"
+    consultas = relationship("Consulta", back_populates="paciente")
 
-    id           = Column("id", Integer, primary_key=True, autoincrement=True)
-    cpf          = Column()
-    nome         = Column()
-    data_nasc    = Column()
-    email        = Column()
-    telefone     = Column()
-    sexo         = Column()
-    endereço     = Column()
-    naturalidade = Column()
-    prontuario   = Column()
+    id                  = Column("id", Integer, primary_key=True, autoincrement=True)
+    cpf                 = Column("cpf", String(14), unique=True, nullable=False)  
+    nome                = Column(String, nullable=False) 
+    data_nasc           = Column(DateTime, nullable=True)  
+    email               = Column(String, nullable=True)
+    telefone            = Column(String(15), nullable=True)
+    sexo                = Column(String(1), nullable=False)
+    endereco            = Column(String, nullable=True)
+    naturalidade        = Column(String, nullable=True)
+    prontuario_numero   = Column("prontuario_numero", ForeignKey('prontuario.id'), nullable=True)
+#================================================================================================
 
-#======================================================================================
-
-#======================================MEDICOS=========================================
 class Medico(Base):
     __tablename__ = "medico"
+    consultas = relationship("Consulta", back_populates="medico")
 
     id              = Column("id", Integer, primary_key=True, autoincrement=True)
-    crm             = Column()
-    nome            = Column()
-    especialidade   = Column()
+    crm             = Column("crm", String(13), unique=True, nullable=False)
+    nome            = Column(String, nullable=False)
+    especialidade   = Column(String, nullable=True)
+#=============================================================================================
 
-#=======================================================================================
-
-#====================================CONSULTA===========================================
 class Consulta(Base):
-    __tablename__= "consulta"
+    __tablename__ = "consulta"
+    medico      = relationship("Medico", back_populates="consultas")
+    paciente    = relationship("Paciente", back_populates="consultas")
 
     id              = Column("id", Integer, primary_key=True, autoincrement=True)
-    data_hora       = Column()
-    status          = Column()
-    paciente_cpf    = Column()
-    medico_crm      = Column()
-
-#====================================DIAGNOSTICO=========================================
-class Diagnostico(Base):
-    __tablename__ = "diagnostico"
-
-    id              = Column("id", Integer, primary_key=True, autoincrement=True)
-    cid             = Column()
-    descrição       = Column()
-    consulta_id     = Column()
-    paciente_cpf    = Column()
-    medico_crm      = Column()
+    data_hora       = Column(DateTime, nullable=False)
+    status          = Column(String, nullable=True)
+    paciente_cpf    = Column("paciente_cpf", ForeignKey('paciente.cpf'), nullable=False)
+    medico_crm      = Column("medico_crm", ForeignKey('medico.crm'), nullable=False)
 #==========================================================================================
 
-#=====================================TRATAMENTO============================================
+class Diagnostico(Base):
+    __tablename__ = "diagnostico"
+    consulta = relationship("Consulta", back_populates="diagnosticos")
+
+    id              = Column("id", Integer, primary_key=True, autoincrement=True)
+    cid             = Column(String, nullable=True)
+    descricao       = Column(String, nullable=True)
+    consulta_id     = Column("consulta_id", ForeignKey('consulta.id'), nullable=False)
+    paciente_cpf    = Column("paciente_cpf", ForeignKey('paciente.cpf'), nullable=False)
+    medico_crm      = Column("medico_crm", ForeignKey('medico.crm'), nullable=False)
+#=========================================================================================
+
 class Tratamento(Base):
     __tablename__ = "tratamento"
 
     id              = Column("id", Integer, primary_key=True, autoincrement=True)
-    descrição       = Column()
-    duração         = Column()
-    medicamentos    = Column()
-    diagnostico     = Column()
+    descricao       = Column(String, nullable=True)
+    duracao         = Column(Integer, nullable=True)
+    medicamentos    = Column(String, nullable=True)
+    diagnostico_id  = Column("diagnostico_id", ForeignKey('diagnostico.id'), nullable=False)
+
 #=========================================================================================
-
-#=====================================RECEITA=============================================
 class Receita(Base):
-    __tabelname__ = "receita"
+    __tablename__ = "receita"
 
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    nome_medico
-#========================================================================================
+    id              = Column("id", Integer, primary_key=True, autoincrement=True)
+    medico_id       = Column(Integer, ForeignKey("medico.id"), nullable=False)
+    descricao       = Column(String, nullable=True)
+    paciente_id     = Column(Integer, ForeignKey("paciente.id"), nullable=False)
+    tratamento_id   = Column("tratamento_id", ForeignKey('tratamento.id'), nullable=True)
+    diagnostico_id  = Column("diagnostico_id", ForeignKey('diagnostico.id'), nullable=True)
+#===========================================================================================
+
 Base.metadata.create_all(bind=db)
 
 #CRUD
